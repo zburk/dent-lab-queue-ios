691af9bc466302ca01d9c77db25e5d3c90f66863
Zachary Burk <zachburk7@gmail.com>
2020-01-21 23:44:55 -0500

    Add landing page and custom colors files

--- DentSimLab/ContentView.swift
+++ DentSimLab/ContentView.swift
@@ -10,12 +10,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .center, spacing: 20) {
+            ButtonOutline(buttonText: "Home")
+
+            ButtonOutline(buttonText: "Queue List")
+
+            ButtonOutline(buttonText: "Join")
+        }
+            .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ButtonOutline: View {
+    var buttonText: String = "";
+
+    var body: some View {
+        Text(buttonText)
+            .fontWeight(.bold)
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding()
+            .foregroundColor(Color.PrimaryBlueColor)
+            .overlay(
+                RoundedRectangle(cornerRadius: 25)
+                    .stroke(Color.PrimaryBlueColor, lineWidth: 5)
+        )
     }
 }
---
+++ DentSimLab/CustomColors.swift
@@ -0,0 +1,13 @@
+//
+//  CustomColors.swift
+//  DentSimLab
+//
+//  Created by Zachary Burk on 1/21/20.
+//  Copyright © 2020 Zachary Burk. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static let PrimaryBlueColor = Color("PrimaryBlue")
+}
